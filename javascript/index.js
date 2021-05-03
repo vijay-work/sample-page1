d2a3647e4efc9c57b283917180ededd304e9f42e
vijay-work <82712827+vijay-work@users.noreply.github.com>
2021-05-03 12:03:43 +0530

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,9 @@
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarNav')
+const bsCollapse = new bootstrap.Collapse(menuToggle)
+bsCollapse.hide();
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
 
 
