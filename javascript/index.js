5044b8798c7126827b90f44ddef94f3750fa2255
vijay-work <82712827+vijay-work@users.noreply.github.com>
2021-05-03 11:55:39 +0530

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -5,12 +5,5 @@ navLinks.forEach((l) => {
     l.addEventListener('click', () => { bsCollapse.toggle() })
 })
 
-function myFunction(x) {
-  if (x.matches) { // If media query matches
-    bsCollapse.hide();
-  }
-}
 
-var x = window.matchMedia("(max-width: 600px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
+bsCollapse.hide();
